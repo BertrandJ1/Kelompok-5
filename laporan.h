93bcc1e98bcd8e6e2cb93c06675f338aefa7b159
unknown <mnurilalam@gmail.com>
2024-11-24 08:41:47 +0700

    d

--- laporan.h
+++
@@ -1,9 +0,0 @@
-// laporan.h
-#ifndef LAPORAN_H
-#define LAPORAN_H
-
-// Deklarasi fungsi untuk laporan penjualan
-void laporanPenjualan();
-
-#endif // LAPORAN_H
-
--- pembayaran.h
+++
@@ -1,9 +0,0 @@
-// pembayaran.h
-#ifndef PEMBAYARAN_H
-#define PEMBAYARAN_H
-
-// Deklarasi fungsi untuk pembayaran
-void pembayaran();
-
-#endif // PEMBAYARAN_H
-
